a8dcf96a7d5b58cd651414ace9a5e48f731cad0f
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 01:16:17 -0500

    Agregar archivos de proyecto.

---
+++ Java-OutPut-Formating/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_OutPut_Formating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("escribe tres entradas");
+            for (int i = 0; i < 3; i++)
+            {
+                String tam1 = Console.ReadLine();
+                int tam2 = Int32.Parse(Console.ReadLine());
+                if (Convert.ToString(tam2).Length == 3)
+                {
+                    Console.WriteLine(tam1 + "  " + tam2);
+                }
+                else if (Convert.ToString(tam2).Length < 3)
+                {
+                    Console.WriteLine(tam1 + "  " + "0" + tam2);
+                }
+            }
+        }
+    }
+}
